d73a455ba3a931e93ed7dbc28be119d8b31fccf6
EnzoD-C <enzo.dutreillycassin@gmail.com>
2025-06-03 15:14:24 +0200

    influx db js

---
+++ influxdb.js
@@ -0,0 +1,50 @@
+import {InfluxDBClient} from '@influxdata/influxdb3-client'
+import {tableFromArrays} from 'apache-arrow';
+
+//Lire les bases de donnees influx db
+const database = process.env.INFLUX_DATABASE;
+const token = process.env.INFLUX_TOKEN;
+const host = "https://us-east-1-1.aws.cloud2.influxdata.com";
+
+async function main() {
+    const client = new InfluxDBClient({host, token})
+    const query = `
+    SELECT
+      room,
+      DATE_BIN(INTERVAL '1 day', time) AS _time,
+      AVG(temp) AS temp,
+      AVG(hum) AS hum,
+      AVG(co) AS co
+    FROM home
+    WHERE time >= now() - INTERVAL '90 days'
+    GROUP BY room, _time
+    ORDER BY _time
+    `
+    const result = await client.query(query, database)
+
+    const data = {room: [], day: [], temp: []}
+
+    for await (const row of result) {
+      data.day.push(new Date(row._time).toISOString())
+      data.room.push(row.room)
+      data.temp.push(row.temp)
+    }
+
+    console.table([...tableFromArrays(data)])
+
+    client.close()
+}
+
+main()
+
+//Ecrire dans les bases de données influx db
+
+async function main() {
+    const client = new InfluxDBClient({host, token})
+
+    const record = "home,room=Living\\ Room temp=22.2,hum=36.4,co=17i"
+    await client.write(record, database)
+    client.close()
+}
+
+main()
